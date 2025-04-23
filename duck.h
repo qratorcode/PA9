c1f78810fe48696c9dec636a40aab1a36b517da0
qratorcode <anciunick@gmail.com>
2025-04-22 18:36:02 -0700

    Create duck.h
    s

---
+++ duck.h
@@ -0,0 +1,33 @@
+#ifndef DUCK_H
+
+#define DUCK_H
+
+#include <SFML/Graphics.hpp>
+#include "entity.h"
+#include <cstdlib>
+#include <ctime>
+
+class duck : public game_entity_inclass {
+private:
+    sf::Vector2f duck_accel;
+    float duck_speed_set;
+    bool duck_alive;
+    bool duck_moving;
+	                                                                  //explained in duck.cpp
+public:
+    duck();
+    ~duck();
+
+    void initialize(const std::string& texturePath, float speed);
+    void spawn(float screenWidth, float screenHeight);
+    void update(float deltaTime, float screenWidth, float screenHeight) override;
+    void render(sf::RenderWindow& window) override;
+
+    bool hit_detect_true(const sf::FloatRect& hitBox);
+    bool duck_flying_current() const { return duck_moving; }
+    bool isAlive() const { return duck_alive; }
+    void kill();
+    sf::FloatRect getBounds() const;
+    sf::Vector2f getPosition() const { return position; }
+}; 
+#endif 
