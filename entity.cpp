70296e41ee1ec5df27c8df905ea6da672143f1af
qratorcode <anciunick@gmail.com>
2025-04-22 18:35:35 -0700

    Create entity.cpp
    nicholas entity.cpp

---
+++ entity.cpp
@@ -0,0 +1,38 @@
+#include "entity.h"
+#include <iostream>
+
+game_entity_inclass::game_entity_inclass() :
+    position(0, 0),                              //set to zero / base values
+    active(false) {
+}
+
+game_entity_inclass::~game_entity_inclass() {
+    
+}          //destructor
+
+
+
+/*
+
+initializes entity with texture
+ texturePath File path to texture asset
+ prints error message if texture fails to load
+ all entities must be initialized before use
+
+*/
+void game_entity_inclass::initialize(const std::string& texturePath) {      
+    if (!texture.loadFromFile(texturePath)) {                                   
+        std::cout << "failed to load texture: " << texturePath << std::endl;
+    }
+    sprite.setTexture(texture);
+}
+// screenWidth and screenHeight are used to set the position of the entity
+void game_entity_inclass::update(float deltaTime, float screenWidth, float screenHeight) {
+    sprite.set_line_position(position);
+}
+
+void game_entity_inclass::render(sf::RenderWindow& window) {
+    if (active) {
+		window.draw(sprite);   //draw at curren t position
+    }
+}
